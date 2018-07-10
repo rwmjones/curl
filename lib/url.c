1873fb38c2f71f6725e69c8ea31fd85a2d033cb1
Richard W.M. Jones <rjones@redhat.com>
2018-07-10 14:56:15 +0100

    nbd: Add NBD (Network Block Device) support.
    Only the NBD "fixed newstyle" protocol is supported, since the
    oldstyle protocol is now obsolete.
    
    This is only a partial implementation of the protocol, enough to
    access non-TLS servers and upload/download data only.  For other
    non-supported items see the list at the top of lib/nbd.c.
    
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>

--- lib/url.c
+++ lib/url.c
@@ -115,6 +115,7 @@ bool curl_win32_idn_to_ascii(const char *in, char **out);
 #include "socks.h"
 #include "curl_rtmp.h"
 #include "gopher.h"
+#include "nbd.h"
 #include "http_proxy.h"
 #include "conncache.h"
 #include "multihandle.h"
@@ -241,6 +242,10 @@ static const struct Curl_handler * const protocols[] = {
   &Curl_handler_rtmpte,
   &Curl_handler_rtmps,
   &Curl_handler_rtmpts,
+#endif
+
+#ifndef CURL_DISABLE_NBD
+  &Curl_handler_nbd,
 #endif
 
   (struct Curl_handler *) NULL
@@ -4199,6 +4204,10 @@ static unsigned int get_protocol_family(unsigned int protocol)
   case CURLPROTO_SMB:
   case CURLPROTO_SMBS:
     family = CURLPROTO_SMB;
+    break;
+
+  case CURLPROTO_NBD:
+    family = CURLPROTO_NBD;
     break;
 
   default:
